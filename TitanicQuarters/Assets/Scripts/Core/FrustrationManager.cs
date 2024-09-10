ae988f81b92dc658cb61d5f8a48d49d5ffb98429
Lucas <l.sarrazin@student.isartdigital.com>
2024-09-10 14:28:29 +0200

    Frustration bar fonctionallity

--- TitanicQuarters/Assets/Scripts/Core/FrustrationManager.cs
+++ TitanicQuarters/Assets/Scripts/Core/FrustrationManager.cs
@@ -1,32 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Slider))]
 public class FrustrationManager : MonoBehaviour
 {
 
-    [SerializeField] // Clamped between 0 and 1
-    float _frustration = 0.0f;
+    private Slider _slider;
 
-    // Start is called before the first frame update
+    // Clamped between 0 and 1
+    float _frustration
+    {
+        get { return _slider.value; }
+        set { _slider.value = value; }
+    }
+
+    [SerializeField]
+    private float _frustrationLossOnWin;
+
+    [SerializeField]
+    [Tooltip("In percents")]
+    public float _frustrationPerSeconds;
+
+
+
     void Start()
     {
-        
+        _slider = GetComponent<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        IncrementFrustration();
     }
 
     public void IncrementFrustration()
     {
         // Increment frustration
+        _frustration += _frustrationPerSeconds / 100f * Time.deltaTime;
     }
 
     public void DecrementFrustration()
     {
         // Decrement frustration
+        _frustration -= _frustrationLossOnWin / 100f;
     }
 }
